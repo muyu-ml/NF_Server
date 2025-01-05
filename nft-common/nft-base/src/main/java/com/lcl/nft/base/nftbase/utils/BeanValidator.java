89d2a28f8d57602275b311e47918572d43b30e3a
conglongli <liconglong@163.com>
2025-01-06 02:10:25 +0800

    feat: 工具类封装

---
+++ nft-common/nft-base/src/main/java/com/lcl/nft/base/nftbase/utils/BeanValidator.java
@@ -0,0 +1,33 @@
+package com.lcl.nft.base.nftbase.utils;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.ValidationException;
+import jakarta.validation.Validator;
+import org.hibernate.validator.HibernateValidator;
+
+import java.util.Set;
+
+/**
+ * 参数校验工具
+ * @Author conglongli
+ * @date 2025/1/6 01:44
+ */
+public class BeanValidator {
+
+    private static Validator validator = Validation.byProvider(HibernateValidator.class).configure().failFast(true)
+            .buildValidatorFactory().getValidator();
+
+    /**
+     *
+     * @param object
+     * @param groups
+     * @throws ValidationException
+     */
+    public static void validateObject(Object object, Class<?>... groups) throws ValidationException {
+        Set<ConstraintViolation<Object>> constraintViolations = validator.validate(object, groups);
+        if (constraintViolations.stream().findFirst().isPresent()) {
+            throw new ValidationException(constraintViolations.stream().findFirst().get().getMessage());
+        }
+    }
+}
