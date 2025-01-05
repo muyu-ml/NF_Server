bb36e90f819780aea00791ae6c5b1aae64e9514a
conglongli <liconglong@163.com>
2025-01-06 00:50:00 +0800

    feat: 定义环境变量配置类

---
+++ nft-common/nft-base/src/main/java/com/lcl/nft/base/nftbase/constant/ProfileConstant.java
@@ -0,0 +1,9 @@
+package com.lcl.nft.base.nftbase.constant;
+
+/**
+ * @Author conglongli
+ * @date 2025/1/6 00:45
+ */
+public class ProfileConstant {
+    public static final String PROFILE_DEV = "dev";
+}
