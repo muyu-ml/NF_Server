062400d1f826d700af2b00939bceca42de3a6446
conglongli <liconglong@163.com>
2025-01-13 01:09:09 +0800

    feat: job init

---
+++ nft-common/nft-job/src/main/java/com/lcl/nft/job/config/XxlJobConfiguration.java
@@ -0,0 +1,39 @@
+package com.lcl.nft.job.config;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author conglongli
+ * @date 2025/1/13 01:03
+ */
+@Configuration
+@EnableConfigurationProperties(XxlJobProperties.class)
+public class XxlJobConfiguration {
+    private static final Logger logger = LoggerFactory.getLogger(XxlJobConfiguration.class);
+
+    @Autowired
+    private XxlJobProperties properties;
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = XxlJobProperties.PREFIX, value = "enable", havingValue = "true")
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(properties.getAdminAddresses());
+        xxlJobSpringExecutor.setAppname(properties.getAppName());
+        xxlJobSpringExecutor.setIp(properties.getIp());
+        xxlJobSpringExecutor.setPort(properties.getPort());
+        xxlJobSpringExecutor.setAccessToken(properties.getAccessToken());
+        xxlJobSpringExecutor.setLogPath(properties.getLogPath());
+        xxlJobSpringExecutor.setLogRetentionDays(properties.getLogRetentionDays());
+        return xxlJobSpringExecutor;
+    }
+}
