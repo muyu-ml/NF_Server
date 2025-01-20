8bbe70b25ee2444e54621f059d57a5a44b93cfe1
conglongli <liconglong@163.com>
2025-01-20 14:48:14 +0800

    feat: user init

---
+++ nft-business/nft-user/src/main/java/com/lcl/nft/user/NftUserApplication.java
@@ -0,0 +1,13 @@
+package com.lcl.nft.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NftUserApplication.class, args);
+    }
+
+}
---
+++ nft-business/nft-user/src/test/java/com/lcl/nft/user/NftUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcl.nft.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
